0fd9df5f145db66e25760265eafda84c474fa6f0
Robin Ricard <ricard.robin@gmail.com>
2017-09-26 05:22:19 +0200

    Integrate with iTerm2 when possible

--- config/zshrc.sh
+++ config/zshrc.sh
@@ -17,3 +17,6 @@ prompt pure
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 fpath=(/usr/local/share/zsh-completions $fpath)
+
+# Load up the iTerm2 shell integration if present
+test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
